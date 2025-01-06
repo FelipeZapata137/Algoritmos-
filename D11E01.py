9ad2cc010ee79f60a2c308f71256d16222521c63
FelipeZapata137 <felipe.zapata@epn.edu.ec>
2025-01-06 11:12:43 -0500

    .

---
+++ D11E01.py
@@ -0,0 +1,6 @@
+elemento1=int(input("Ingrese el elemento 1 (Int): "))
+elemento2=input("Ingrese el elemento 2 (String): ")
+elemento3=list(input("Ingrese una lista: "))
+
+tupla=(elemento1,elemento2,elemento3)
+print(f"Mi tupla es: {tupla}")
